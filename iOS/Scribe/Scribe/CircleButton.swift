376c8a96cfe60c7c7b3c0a26b18b99f4eee0bcaa
FrankGarcia_ <futboler101@yahoo.com>
2016-12-22 10:03:19 -0800

    added a speech recognizer app, only works on device

---
+++ iOS/Scribe/Scribe/CircleButton.swift
@@ -0,0 +1,28 @@
+//
+//  CircleButton.swift
+//  Scribe
+//
+//  Created by Frank Garcia on 12/21/16.
+//  Copyright © 2016 Malibit. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 30.0 {
+        didSet {
+            setupView()
+        }
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setupView()
+    }
+    
+    func setupView() {
+        layer.cornerRadius = cornerRadius
+    }
+
+}
